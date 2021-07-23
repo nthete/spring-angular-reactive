34fd5e8220e67a877f5f2ce188361ae1c1916395
nilesh thete <nthete@shutterfly.com>
2021-07-23 08:09:11 +0530

    Removing service

--- stcok-backend/src/main/java/com/clairvoyant/stcokbackend/controller/StocksPriceController.java
+++ stcok-backend/src/main/java/com/clairvoyant/stcokbackend/controller/StocksPriceController.java
@@ -2,7 +2,6 @@ package com.clairvoyant.stcokbackend.controller;
 
 import com.clairvoyant.stcokbackend.model.StockPrice;
 import com.clairvoyant.stcokbackend.repository.StockPriceRepository;
-import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequestMapping("/api/stocks-price")
@@ -23,8 +21,6 @@ public class StocksPriceController {
 
   @GetMapping(value = "/{name}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   public Flux<StockPrice> streamTickerPrice(@PathVariable String name) {
-    return stockPriceRepository.findByStockName(name)
-        .repeatWhen(flux -> flux.delayElements(Duration.ofSeconds(1)))
-        .subscribeOn(Schedulers.boundedElastic());
+    return stockPriceRepository.findByStockName(name);
   }
 }
